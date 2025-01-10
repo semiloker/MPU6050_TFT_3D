ebc2c4ffb2b1841f8c009e4fbf4c7dbd1a9db8ca
Andrii <smlkr1294@gmail.com>
2025-01-10 22:14:00 +0200

    plane

--- src/main.cpp
+++ src/main.cpp
@@ -107,10 +107,66 @@ void drawText(float ax, float ay, float az, float gx, float gy, float gz, float 
   tft.print("Temp: "); tft.print(temp, 1); tft.print("C");
 }
 
+void drawCustomCube(float x, float y, float z, float width, float height, float depth, float rotationX, float rotationY, float lastTransformed[8][2]) 
+{
+    float localCube[8][3] = {
+        {x - width / 2, y - height / 2, z - depth / 2}, {x + width / 2, y - height / 2, z - depth / 2},
+        {x + width / 2, y + height / 2, z - depth / 2}, {x - width / 2, y + height / 2, z - depth / 2},
+        {x - width / 2, y - height / 2, z + depth / 2}, {x + width / 2, y - height / 2, z + depth / 2},
+        {x + width / 2, y + height / 2, z + depth / 2}, {x - width / 2, y + height / 2, z + depth / 2}
+    };
+
+    float transformed[8][2];
+    for (int i = 0; i < 8; i++) 
+    {
+        float newY = localCube[i][1] * cos(rotationX) - localCube[i][2] * sin(rotationX);
+        float newZ = localCube[i][1] * sin(rotationX) + localCube[i][2] * cos(rotationX);
+        float newX = localCube[i][0] * cos(rotationY) + newZ * sin(rotationY);
+        newZ = -localCube[i][0] * sin(rotationY) + newZ * cos(rotationY);
+
+        transformed[i][0] = newX * (tft.width() / 2) / (newZ + 100) + tft.width() / 2;
+        transformed[i][1] = newY * (tft.height() / 2) / (newZ + 100) + tft.height() / 2;
+    }
+
+    for (int i = 0; i < 12; i++) 
+    {
+        int start = edges[i][0];
+        int end = edges[i][1];
+
+        if (lastTransformed[start][0] >= 0 && lastTransformed[end][0] >= 0) 
+        {
+            tft.drawLine(
+                lastTransformed[start][0], lastTransformed[start][1],
+                lastTransformed[end][0], lastTransformed[end][1],
+                ST77XX_BLACK
+            );
+        }
+
+        tft.drawLine(
+            transformed[start][0], transformed[start][1],
+            transformed[end][0], transformed[end][1],
+            ST77XX_WHITE
+        );
+    }
+
+    for (int i = 0; i < 8; i++) 
+    {
+        lastTransformed[i][0] = transformed[i][0];
+        lastTransformed[i][1] = transformed[i][1];
+    }
+}
+
 void loop() 
 {
   static float rotationX = 0, rotationY = 0;
-  static float lastTransformed[8][2] = {0};
+  static float lastTransformed[8][2];
+  static float lastTransformed1[8][2];
+  static float lastTransformed2[8][2];
+
+  static float lastTransformed3[8][2];
+  static float lastTransformed4[8][2];
+
+  static float lastTransformed5[8][2];
 
   sensors_event_t a, g, temp;
   mpu.getEvent(&a, &g, &temp);
@@ -118,7 +174,16 @@ void loop()
   rotationX += g.gyro.y * 0.05;
   rotationY += g.gyro.x * 0.05;
 
-  drawCube(rotationY, rotationX, lastTransformed);
+  drawCustomCube(0, 0, 0, 15, 10, 60, rotationY, rotationX, lastTransformed);
+  drawCustomCube(-25, 0, 0, 30, 10, 5, rotationY, rotationX, lastTransformed1);
+  drawCustomCube(25, 0, 0, 30, 10, 5, rotationY, rotationX, lastTransformed2);
+
+  drawCustomCube(-15, 0, -25, 15, 8, 5, rotationY, rotationX, lastTransformed3);
+  drawCustomCube(15, 0, -25, 15, 8, 5, rotationY, rotationX, lastTransformed4);
+
+  drawCustomCube(0, -10, -25, 4, 12, 5, rotationY, rotationX, lastTransformed5);
+
+  // drawCube(rotationY, rotationX, lastTransformed);
   drawText(a.acceleration.x, a.acceleration.y, a.acceleration.z, g.gyro.x, g.gyro.y, g.gyro.z, temp.temperature);
 
   delay(50);
